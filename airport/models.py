e84a60d312b09c4940afb4dcd2856b09269b1e21
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-03-24 09:07:49 +0100

    feat: update related names for Route and Order models, optimize queryset in RouteViewSet

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,127 @@
 from django.db import models
+from datetime import timedelta
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
+from django.utils import timezone
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="source_routes"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="destination_routes"
+    )
+
+    def __str__(self):
+        return f"{self.source} -> {self.destination}"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField(validators=[MinValueValidator(1)])
+    seats_in_row = models.IntegerField(validators=[MinValueValidator(1)])
+    airplane_type = models.ForeignKey(
+        AirplaneType, on_delete=models.CASCADE, related_name="airplane"
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.airplane_type.name}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="order",
+    )
+
+    def __str__(self):
+        return f"{self.user} {self.created_at}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="flight"
+    )
+    airplane = models.ForeignKey(
+        Airplane, on_delete=models.CASCADE, related_name="flight"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew, blank=True, related_name="flight")
+
+    def clean(self):
+        super().clean()
+
+        now_with_margin = timezone.now() - timedelta(minutes=2)
+
+        if self.departure_time and self.arrival_time:
+            if self.departure_time >= self.arrival_time:
+                raise ValidationError(
+                    {
+                        "arrival_time": (
+                            "arrival time need to be after departure"
+                            "(can't be on same time)"
+                        )
+                    }
+                )
+            if self.departure_time < now_with_margin:
+                raise ValidationError(
+                    {"departure_time": "departure time can't be in pass"}
+                )
+
+    def __str__(self):
+        return (
+            f"{self.route} {self.airplane} "
+            f"{self.departure_time} {self.arrival_time}"
+        )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("row", "seat", "flight", "order")
+        ordering = ["flight", "row", "seat"]
+
+    def clean(self):
+        if self.row > self.flight.airplane.rows:
+            raise ValidationError(f"Row {self.row} out of range")
+        if self.seat > self.flight.airplane.seats_in_row:
+            raise ValidationError(f"Seat {self.seat} out of range")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.row} {self.seat} {self.flight} {self.order}"
--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    Crew,
+    Airport,
+    Route,
+    AirplaneType,
+    Order,
+    Flight,
+    Ticket,
+)
+from airport.serializers import (
+    CrewSerializer,
+    AirportSerializer,
+    RouteSerializer,
+    AirplaneTypeSerializer,
+    OrderSerializer,
+    FlightSerializer,
+    TicketSerializer,
+)
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all().select_related(
+        "source_routes", "destination_routes"
+    )
+    serializer_class = RouteSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all().select_related()
+    serializer_class = AirplaneTypeSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
